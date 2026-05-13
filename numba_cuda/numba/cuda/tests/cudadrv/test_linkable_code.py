bed0ab6f4fd4bf9455dd51675a1d71dcabcfe56e
Andres Guzman-Ballen <acosmicflamingo@gmail.com>
2026-05-13 15:21:30 -0500

    Migrate cudadrv/test_linkable_code to pytest
    Tweak approach to instantiating test_device_function path to make it
    possible to use pytest.mark.parametrize instead of iterate through
    names within test.

--- numba_cuda/numba/cuda/tests/cudadrv/test_linkable_code.py
+++ numba_cuda/numba/cuda/tests/cudadrv/test_linkable_code.py
@@ -2,44 +2,40 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 import os
+
+import pytest
+
 from numba import cuda
 from numba.cuda.cudadrv.linkable_code import LinkableCode
-from numba.cuda.testing import CUDATestCase, skip_on_cudasim
-import unittest
+from numba.cuda.testing import skip_on_cudasim
 
 TEST_BIN_DIR = os.getenv("NUMBA_CUDA_TEST_BIN_DIR")
-if TEST_BIN_DIR:
-    test_device_functions_a = os.path.join(
-        TEST_BIN_DIR, "test_device_functions.a"
-    )
-    test_device_functions_cubin = os.path.join(
-        TEST_BIN_DIR, "test_device_functions.cubin"
-    )
-    test_device_functions_cu = os.path.join(
-        TEST_BIN_DIR, "test_device_functions.cu"
-    )
-    test_device_functions_fatbin = os.path.join(
-        TEST_BIN_DIR, "test_device_functions.fatbin"
-    )
-    test_device_functions_fatbin_multi = os.path.join(
-        TEST_BIN_DIR, "test_device_functions_multi.fatbin"
-    )
-    test_device_functions_o = os.path.join(
-        TEST_BIN_DIR, "test_device_functions.o"
-    )
-    test_device_functions_ptx = os.path.join(
-        TEST_BIN_DIR, "test_device_functions.ptx"
-    )
-    test_device_functions_ltoir = os.path.join(
-        TEST_BIN_DIR, "test_device_functions.ltoir"
-    )
 
 
-class TestLinkableCode(CUDATestCase):
+def bin_path(filename):
+    return os.path.join(TEST_BIN_DIR, filename) if TEST_BIN_DIR else None
+
+
+test_device_functions_a = bin_path("test_device_functions.a")
+test_device_functions_cubin = bin_path("test_device_functions.cubin")
+test_device_functions_cu = bin_path("test_device_functions.cu")
+test_device_functions_fatbin = bin_path("test_device_functions.fatbin")
+test_device_functions_fatbin_multi = bin_path(
+    "test_device_functions_multi.fatbin"
+)
+test_device_functions_o = bin_path("test_device_functions.o")
+test_device_functions_ptx = bin_path("test_device_functions.ptx")
+test_device_functions_ltoir = bin_path("test_device_functions.ltoir")
+
+
+class TestLinkableCode:
     @skip_on_cudasim(reason="Simulator does not support linkable code")
-    @unittest.skipIf(not TEST_BIN_DIR, "necessary binaries not generated.")
-    def test_linkable_code_from_path_or_obj(self):
-        files_kind = [
+    @pytest.mark.skipif(
+        not TEST_BIN_DIR, reason="necessary binaries not generated."
+    )
+    @pytest.mark.parametrize(
+        "path,kind",
+        [
             (test_device_functions_a, cuda.Archive),
             (test_device_functions_cubin, cuda.Cubin),
             (test_device_functions_cu, cuda.CUSource),
@@ -47,12 +43,12 @@ class TestLinkableCode(CUDATestCase):
             (test_device_functions_o, cuda.Object),
             (test_device_functions_ptx, cuda.PTXSource),
             (test_device_functions_ltoir, cuda.LTOIR),
-        ]
+        ],
+    )
+    def test_linkable_code_from_path_or_obj(self, path, kind):
+        obj = LinkableCode.from_path_or_obj(path)
+        assert isinstance(obj, kind)
 
-        for path, kind in files_kind:
-            obj = LinkableCode.from_path_or_obj(path)
-            assert isinstance(obj, kind)
-
-            # test identity of from_path_or_obj
-            obj2 = LinkableCode.from_path_or_obj(obj)
-            assert obj2 is obj
+        # test identity of from_path_or_obj
+        obj2 = LinkableCode.from_path_or_obj(obj)
+        assert obj2 is obj
